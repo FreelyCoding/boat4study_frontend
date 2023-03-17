dc686a4917d25d706f158c17fad39c6401ca8f0d
scongl <sicongli123@163.com>
2023-03-17 23:28:15 +0800

    做题界面大致设计

---
+++ freelycoding/App.vue
@@ -0,0 +1,20 @@
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+</style>
+
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+
---
+++ freelycoding/pages/navbar/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<u-navbar title="" :bgColor="bgColor">
+			
+		</u-navbar>
+		<view class="content">
+			<!-- 正文内容 -->
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bgColor: '#001f3f',
+			}
+		}
+	}
+</script>
