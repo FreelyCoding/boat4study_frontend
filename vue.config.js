d96c9e5a99a7badcb632c6026f73173f05ac128b
scongl <sicongli123@163.com>
2023-03-23 22:40:16 +0800

    登录页面demo，仅支持在uniapp内置浏览器观看效果

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
     // 配置路径别名
     configureWebpack: {
         devServer: {
+			proxy: {
+			  '/prefix/api/user/list': {
+				target: 'http://123.249.3.32',
+				pathRewrite: {
+				  '^/prefix': ''
+				}
+			  }
+			},
+					
             // 调试时允许内网穿透，让外网的人访问到本地调试的H5页面
             disableHostCheck: true
         }
